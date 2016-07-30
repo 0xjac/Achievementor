99232daf29ce26b51f5ccafc3857fada4fc138ed
Samantha Rosso <sammyrosso@hotmail.com>
2016-07-30 20:21:57 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 "use strict";
 
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const dust = require('adaro');
+const dust         = require('adaro');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const logger       = require('morgan');
+const path         = require('path');
 
 const routes = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
-
-console.log(process.env.NODE_ENV);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
